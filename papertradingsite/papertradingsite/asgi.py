8b635fc534b6acb3e5e92f035d245ff91e2267a8
Elam W <tedmanz2003@gmail.com>
2024-04-16 14:37:11 -0500

    resetting to create a VE

--- papertradingsite/papertradingsite/asgi.py
+++ papertradingsite/papertradingsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for papertrader project.
+ASGI config for papertradingsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papertrader.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papertradingsite.settings')
 
 application = get_asgi_application()
--- papertradingsite/papertradingsite/wsgi.py
+++ papertradingsite/papertradingsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for papertrader project.
+WSGI config for papertradingsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papertrader.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papertradingsite.settings')
 
 application = get_wsgi_application()
