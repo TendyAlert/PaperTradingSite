9b096e5ccda9654996db4a489c5f3663a7cc3f77
Elam W <tedmanz2003@gmail.com>
2024-04-17 16:16:41 -0500

    Removed UserBalance model and linked portfolio to user auth Users

--- papertradingsite/papertradingapp/admin.py
+++ papertradingsite/papertradingapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin #type: ignore
-from .models import UserBalance, Stock
+from .models import Stock
 
 # Register your models here.
-admin.site.register(UserBalance)
 admin.site.register(Stock)
--- papertradingsite/papertradingapp/models.py
+++ papertradingsite/papertradingapp/models.py
@@ -1,18 +1,11 @@
 from django.db import models # type: ignore
-
-nl = '\n'
+from django.contrib.auth.models import User #type: ignore
 
 # Create your models here.
-class UserBalance(models.Model):
-    first_name = models.TextField(max_length=70)
-    last_name = models.TextField(max_length=70)
-    balance = models.DecimalField(default=2000.00, decimal_places=2, max_digits=9)
-    
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
     
 class Stock(models.Model):
-    user_balance = models.ForeignKey(UserBalance, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user_balance = models.DecimalField(decimal_places=2, default=2000.00, max_digits=9)
     stock_ticker = models.CharField(max_length=4)
     quantity = models.IntegerField(default=0)
     stock_value = models.DecimalField(decimal_places=2, max_digits=9)
